81794a32714ad71173d76accd62b32e15b4d3252
Mathis Côté <mathis.cote1@outlook.com>
2023-07-08 11:02:07 -0400

    feat: add prisma and sqlite database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const users = await prisma.user.findMany();
+  console.log(users);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Button</Button>
