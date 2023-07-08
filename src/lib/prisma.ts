1a3a21da99f11359488790ebcdacd4961c4c2408
luka0009 <109582754+luka0009@users.noreply.github.com>
2023-07-08 21:25:24 +0400

    instantiating PrismaClient
    Based on the best practices outlined in the Prisma documentation, I discovered that the current method of instantiating the PrismaClient using the app directory could potentially lead to some issues. To ensure a more robust and reliable implementation, I recommend adopting this approach as outlined in official Prisma docs.
    
    You can read more about this in Prisma documentation: https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
