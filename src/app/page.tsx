00ac8e50ad536f1a29d20e03cc44c74c52ed201b
harmochiky <harmochiky2@gmail.com>
2023-07-09 01:07:48 +0200

    Created Dashboard Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/prisma";
 
 export default async function Home() {
-  const users = await prisma.user.findMany();
-  console.log(users);
+  // Commented out to remove env error
+  // const users = await prisma.user.findMany();
+  // console.log(users);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
