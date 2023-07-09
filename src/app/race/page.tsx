58cc1f3dce13e22516bbb16b94e745c49d7ea755
scape76 <danyaolekhq@gmail.com>
2023-07-09 19:55:46 +0300

    add handy getUser function

--- src/app/race/page.tsx
+++ src/app/race/page.tsx
@@ -1,6 +1,7 @@
 import TypingCode from "@/components/typing/typingCode";
 
 export default async function Race() {
+
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
