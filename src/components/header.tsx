ce6eff6456fe802bf0912f677f52b8fd15dc81b5
Web Dev Cody <webdevcody@gmail.com>
2023-07-09 11:13:08 -0400

    chore: fixing issue with javascript:void

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+
 import { Button } from "./ui/button";
 
 const Header = () => {
@@ -19,7 +20,7 @@ const Header = () => {
       <nav className="bg-white w-full border-b md:border-0 md:static">
         <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <Link href="javascript:void(0)">
+            <Link href="/">
               <Image
                 unoptimized
                 src="/static/logo.png"
