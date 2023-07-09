a41ee93f037bf7ebe612ddfd3dffca0b7e25a704
scape76 <danyaolekhq@gmail.com>
2023-07-09 20:48:43 +0300

    pass user to typingCode comp and run action when user's done with the challange

--- src/app/race/page.tsx
+++ src/app/race/page.tsx
@@ -1,11 +1,15 @@
 import TypingCode from "@/components/typing/typingCode";
+import { getCurrentUser } from "@/lib/session";
 
 export default async function Race() {
+  const user = await getCurrentUser();
+
+  console.log(user);
 
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <TypingCode />
+        <TypingCode user={user}/>
       </main>
     </>
   );
