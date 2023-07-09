19248e477cf64d755066f383ef3c5500b8efe934
scape76 <danyaolekhq@gmail.com>
2023-07-09 20:47:28 +0300

    add session stategy option and secret val

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,10 @@ import GithubProvider from "next-auth/providers/github";
 
 export const nextAuthOptions = {
   adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET!,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
