82787386a97516d5a1729c0c467556684f445858
Ragudos <workemailaaronragudos@gmail.com>
2023-07-11 02:38:04 +0800

    Changes to be committed: 	new file:   src/app/(profile-related)/profile/_components/change-name-form.tsx 	new file:   src/app/(profile-related)/profile/_components/confirmation.tsx 	new file:   src/app/(profile-related)/profile/_components/index.ts 	new file:   src/app/(profile-related)/profile/_components/profile-nav.tsx 	renamed:    src/app/profile/_components/search-param-pfp.tsx -> src/app/(profile-related)/profile/_components/search-param-pfp.tsx 	renamed:    src/app/profile/actions.ts -> src/app/(profile-related)/profile/actions.ts 	renamed:    src/app/profile/loading.tsx -> src/app/(profile-related)/profile/loading.tsx 	new file:   src/app/(profile-related)/profile/page.tsx 	renamed:    src/app/profile/view-photo/page.tsx -> src/app/(profile-related)/profile/view-photo/page.tsx 	deleted:    src/app/profile/_components/change-name-form.tsx 	deleted:    src/app/profile/_components/index.ts 	deleted:    src/app/profile/_components/profile-image-button.tsx 	deleted:    src/app/profile/_components/profile-navigation.tsx 	deleted:    src/app/profile/_components/toggle-change-name.tsx 	deleted:    src/app/profile/layout.tsx 	deleted:    src/app/profile/page.tsx 	deleted:    src/app/profile/race-history/loading.tsx 	deleted:    src/app/profile/race-history/page.tsx 	new file:   src/components/ui/editable-input.tsx 	modified:   tailwind.config.js

---
+++ src/app/(profile-related)/profile/_components/index.ts
@@ -0,0 +1,6 @@
+import ProfileNav from "./profile-nav";
+import ChangeNameForm from "./change-name-form";
+export {
+  ProfileNav,
+  ChangeNameForm,
+};
--- src/app/(profile-related)/profile/actions.ts
+++ src/app/(profile-related)/profile/actions.ts
@@ -2,6 +2,7 @@
 
 import { prisma } from "@/lib/prisma";
 import { getCurrentUser } from "@/lib/session";
+import { signOut } from "next-auth/react";
 
 export async function updateUserAction({ newName }: { newName: string }) {
   const user = await getCurrentUser();
@@ -23,3 +24,21 @@ export async function updateUserAction({ newName }: { newName: string }) {
     },
   });
 }
+
+export async function deleteUserAction({ userId }: { userId: string; }) {
+  const user = await getCurrentUser();
+  
+  if (!user) {
+    return new Response("user-not-defined", { status: 401 });
+  }
+
+  if (userId !== user.id) {
+    return new Response("action-not-authorized", { status: 401 });
+  }
+
+  await prisma.user.delete({
+    where: {
+      id: userId
+    }
+  });
+};
