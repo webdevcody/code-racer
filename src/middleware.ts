e3d1017167ad4e21002eead0275ace469688bb4c
Web Dev Cody <webdevcody@gmail.com>
2023-07-10 16:26:36 -0400

    revert middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,2 @@
-import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
-
-export default withAuth(
-  function middleware(request: NextRequestWithAuth) {
-    if (request.nextUrl.pathname.startsWith("/middleware")) {
-      return NextResponse.rewrite(new URL("/", request.url));
-    }
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
-  }
-);
-
-export const config = { matcher: ["/dashboard"] };
+export { default } from "next-auth/middleware";
+export const config = { matcher: ["/dashboard/:path*"] };
