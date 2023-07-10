af48bb762f2e1cfc1d639f81d0d5eaea5ec19eb2
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-07-10 21:09:01 +0530

    middleware fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,3 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+export { default } from "next-auth/middleware"
 
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
-
-  if (!token) {
-    return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
-  }
-}
-
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: ["/dashboard/:path*"],
-};
+export const config = { matcher: ["/dashboard/path*"] }
