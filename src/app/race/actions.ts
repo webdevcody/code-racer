efec9b7118845188719fa4478f7e9e92e224596d
scape76 <danyaolekhq@gmail.com>
2023-07-10 15:05:12 +0300

    72 task: Mock the calculations of accuracy and wpm to prevent error

--- src/app/race/actions.ts
+++ src/app/race/actions.ts
@@ -1,15 +1,21 @@
 "use server";
 
-import { User } from "@prisma/client";
+import { User, Prisma } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
 
 export async function saveUserResult(input: {
   userId: User["id"];
   timeTaken: string | number;
+  errors: number;
+  wpm: number;
+  accuracy: number;
 }) {
   if (
-    typeof input.timeTaken !== "string" &&
-    typeof input.timeTaken !== "number"
+    (typeof input.timeTaken !== "string" &&
+      typeof input.timeTaken !== "number") ||
+    typeof input.errors !== "number" ||
+    typeof input.wpm !== "number" ||
+    typeof input.accuracy !== "number"
   ) {
     throw new Error("Invalid input.");
   }
@@ -18,6 +24,9 @@ export async function saveUserResult(input: {
     data: {
       userId: input.userId,
       takenTime: input.timeTaken.toString(),
+      errors: input.errors,
+      wpm: input.wpm,
+      accuracy: new Prisma.Decimal(input.accuracy),
     },
   });
 }
