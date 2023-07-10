fc4bc2ef7fb8cbda858714f759b959eaf43d87ae
shardel23 <shardel23@gmail.com>
2023-07-11 00:06:17 +0300

    fix format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextTopLoader showSpinner={false}/>
+        <NextTopLoader showSpinner={false} />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
           {children}
