3562618fa2b7e1258ccec2065118f82826e9f726
Web Dev Cody <webdevcody@gmail.com>
2023-07-11 16:02:08 -0400

    fixing deployed env

--- prisma/migrations/20230708174655_migrate_user/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "name" TEXT,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
