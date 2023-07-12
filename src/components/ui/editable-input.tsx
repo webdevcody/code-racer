6737ffd4e8fa29ea3839c0221663648d29762bfc
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-07-12 22:28:30 +0530

    have it dynamic with overflow hidden

--- src/components/ui/editable-input.tsx
+++ src/components/ui/editable-input.tsx
@@ -92,7 +92,7 @@ const EditableInput = React.forwardRef<HTMLInputElement, EditableInputProps>(
             title={`Change current username: ${value}`}
             role="button"
             className={cn(
-              "bg-background h-10 px-3 py-2 w-full text-base",
+              "bg-background h-fit px-3 py-2 w-full text-base overflow-hidden",
               className,
             )}
           >
