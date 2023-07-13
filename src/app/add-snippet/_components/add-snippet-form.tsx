44eadc3f306bb54f6bee4a2cb22d9ef5ddf5150c
Vignesh <vighneshgupta32@gmail.com>
2023-07-13 12:30:34 +0530

    [added] textarea shadcn

--- src/app/add-snippet/_components/add-snippet-form.tsx
+++ src/app/add-snippet/_components/add-snippet-form.tsx
@@ -1,28 +1,29 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import React, { useState } from "react";
 import { addSnippetAction } from "../actions";
+import { Textarea } from "@/components/ui/textarea";
 
-const snippetLangs = [
-  { name: "C/C++" },
-  { name: "C#" },
-  { name: "Go" },
-  { name: "HTML" },
-  { name: "Java" },
-  { name: "Javascript" },
-  { name: "PHP" },
-  { name: "Python" },
-  { name: "Ruby" },
-  { name: "Swift" },
-  { name: "Typescript" },
+const snippetLangs: { name: string; value: string }[] = [
+  { name: "C/C++", value: "c_cpp" },
+  { name: "C#", value: "csharp" },
+  { name: "Go", value: "go" },
+  { name: "HTML", value: "html" },
+  { name: "Java", value: "java" },
+  { name: "Javascript", value: "javascript" },
+  { name: "PHP", value: "php" },
+  { name: "Python", value: "python" },
+  { name: "Ruby", value: "ruby" },
+  { name: "Swift", value: "swift" },
+  { name: "Typescript", value: "typescript" },
 ];
 
 export default function AddSnippetForm({}) {
   const [codeSnippet, setCodeSnippet] = useState("");
   const [codeLanguage, setCodeLanguage] = useState("");
 
-  async function handleSubmit(e: any) {
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     await addSnippetAction({
       language: codeLanguage,
@@ -41,30 +42,33 @@ export default function AddSnippetForm({}) {
     <form
       onSubmit={handleSubmit}
       action=""
-      className="mt-5 flex flex-col gap-3"
+      className="flex flex-col gap-3 mt-5"
     >
       <div>
         <select
           onChange={(e) => setCodeLanguage(e.target.value)}
-          className="py-3 px-4 w-full"
+          className="w-full px-4 py-3"
           name=""
           id=""
         >
           <option value="select">Select Snippet Language</option>
-          <option value="typescript">Typescript</option>
-          <option value="python">Python</option>
-          <option value="javascript">Javascript</option>
+          {snippetLangs.map((lang) => (
+            <option key={lang.value} value={lang.value}>
+              {lang.name}
+            </option>
+          ))}
         </select>
       </div>
       <div>
-        <textarea
+        <Textarea
           onChange={(e) => setCodeSnippet(e.target.value)}
           name=""
           value={codeSnippet}
           id=""
           rows={8}
-          className="border p-2 w-full"
-        ></textarea>
+          className="w-full p-2 border"
+          placeholder="Type your message here."
+        />
       </div>
       <Button className="w-fit">Upload</Button>
     </form>
