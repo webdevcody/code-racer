e2a691a1658a88d77f621f4f97ad5065b2cb2479
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-07-13 10:19:19 -0700

    Fix Merge
    -change function name

--- src/app/race/typingCode.tsx
+++ src/app/race/typingCode.tsx
@@ -252,7 +252,7 @@ export default function TypingCode({ user, snippet }: TypingCodeProps) {
   return (
     <div
       className="w-3/4 lg:p-8 p-4 bg-accent rounded-md relative"
-      onClick={focusOnCode}
+      onClick={focusOnLoad}
       role="none" // eslint fix - will remove the semantic meaning of an element while still exposing it to assistive technology
     >
       <RacePositionTracker
