d15930024085db2e906a71a708decbe35efe901c
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-07-13 13:52:01 +0530

    changed the position of div with buttons

--- src/components/ui/editable-input.tsx
+++ src/components/ui/editable-input.tsx
@@ -61,7 +61,7 @@ const EditableInput = React.forwardRef<HTMLInputElement, EditableInputProps>(
               autoFocus
               {...props}
             />
-            <div className="absolute top-0 right-0 flex items-center justify-center w-24 h-full gap-2 bg-background">
+            <div className="absolute top-11 right-0 flex items-center w-full justify-evenly h-full gap-2 bg-background">
               <button
                 type="button"
                 className="relative w-4 h-4"
