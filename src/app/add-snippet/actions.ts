bd778a6965506080d3de94bed4c40d3616fe61d3
dekomori09 <dekomori09@gmail.com>
2023-07-14 23:11:20 +0530

    221 ts issues

--- src/app/add-snippet/actions.ts
+++ src/app/add-snippet/actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
+import { UnauthorizedError } from "@/lib/exceptions/custom-hooks";
 import { prisma } from "@/lib/prisma";
+import { getCurrentUser } from "@/lib/session";
 import type { Snippet } from "@prisma/client";
 
 async function hasUserCreatedSnippet(userId: string): Promise<boolean> {
@@ -50,6 +52,12 @@ export async function addSnippetAction({
   code,
   language,
 }: Pick<Snippet, "code" | "language">) {
+
+  const user = await getCurrentUser();
+
+  if (!user) {
+    throw new UnauthorizedError();
+  }
 
   // why am i getting errors here with where?
   await prisma.snippet.create({
