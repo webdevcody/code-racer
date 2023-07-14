a1a0b9e5ec81d4b9028d7404193f6fc6b5980858
greidinger-reis <jp.greidinger@hotmail.com>
2023-07-14 14:48:38 -0300

    fix param name

--- src/app/race/multiplayer/[roomId]/page.tsx
+++ src/app/race/multiplayer/[roomId]/page.tsx
@@ -1,3 +1,7 @@
-export default function PrivateRacePage({ roomId }: { roomId: string }) {
+export default function PrivateRacePage({
+  params,
+}: {
+  params: { roomId: string };
+}) {
   return <div>Private room page not implemented</div>;
 }
