7b1b5c1ebc05bc6f35548a106f85fa66b0d5e0f7
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-07-15 12:12:30 +0530

    added subheading component and privacy page layout

--- src/components/add-bio.tsx
+++ src/components/add-bio.tsx
@@ -8,7 +8,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "./ui/textarea";
 
