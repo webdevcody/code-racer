b3915c3edb45f47d7258df64c94d26723347958d
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-07-16 00:30:51 +0400

    simplify type

--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -1,17 +1,15 @@
 type HeadingProps = ({
   title: string
-} & {
   description?: string
-}) | {
-  description: string
-} & {
+}) | ({
   title?: string
-}
+  description: string
+})
 
 export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
   return (
     <div>
-      {title &&<h2 className="text-2xl md:text-4xl font-bold tracking-tight">{title}</h2>}
+      {title && <h2 className="text-2xl md:text-4xl font-bold tracking-tight">{title}</h2>}
       {description && <p className="text-sm md:text-base text-muted-foreground">{description}</p>}
     </div>
   );
