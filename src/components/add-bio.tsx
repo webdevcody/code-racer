c30d1209d0b41b4e353c6f610993366a80355aa5
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-07-15 11:43:16 +0530

    created the front-end part to add bio

---
+++ src/components/add-bio.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "./ui/textarea";
+
+export function AddBio() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Your Bio</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Bio</DialogTitle>
+          <DialogDescription>
+            Tell us something about you. Only share information you are
+            comfortable with. This will be public information.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+            Biography
+            </Label>
+          </div>
+          <Textarea placeholder="Add your bio" />
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
