7a6e39ca54c8562c8e23ba60719c50a9f6644869
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-07-16 00:11:17 +0400

    refactor footer, nav, heading, site config, icons

--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -1,13 +1,18 @@
-interface HeadingProps {
-  title: string;
-  description: string;
+type HeadingProps = ({
+  title: string
+} & {
+  description?: string
+}) | {
+  description: string
+} & {
+  title?: string
 }
 
 export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
   return (
     <div>
-      <h2 className="text-2xl md:text-4xl font-bold tracking-tight">{title}</h2>
-      <p className="text-sm md:text-base text-muted-foreground">{description}</p>
+      {title &&<h2 className="text-2xl md:text-4xl font-bold tracking-tight">{title}</h2>}
+      {description && <p className="text-sm md:text-base text-muted-foreground">{description}</p>}
     </div>
   );
 };
