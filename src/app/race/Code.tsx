910deda78b3f8b4f4b9b3f71fcbb87b28886a602
Lucas LaRocco <llarocco399@hotmail.com>
2023-07-15 22:29:47 -0500

    Added a smooth follower when the user types text

--- src/app/race/Code.tsx
+++ src/app/race/Code.tsx
@@ -1,18 +1,24 @@
+import { useRef } from "react";
 import { cn } from "@/lib/utils";
+import Follower from "./Follower";
+
 interface CodeProps {
   code: string;
   userInput: string;
   errors: number[];
 }
 export default function Code({ code, errors, userInput }: CodeProps) {
+  const codeContainerRef = useRef<HTMLPreElement>(null);
+
   return (
-    <pre className="text-primary mb-4 overflow-auto">
+    <pre ref={codeContainerRef} className="text-primary mb-4 overflow-auto">
+      <Follower codeContainerRef={codeContainerRef} userInput={userInput} />
       {code.split("").map((char, index) => (
         <span
           key={index}
           className={cn({
             "text-red-500 opacity-100": errors.includes(index),
-            "bg-yellow-200 opacity-80 text-black": userInput.length === index,
+            "opacity-70 ": userInput.length === index,
             "opacity-100":
               userInput.length !== index && userInput[index] === char,
             "opacity-50":
