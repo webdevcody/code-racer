f7c11cf811a36993c40cd09468b8cfd364982027
PatalJunior <81658559+PatalJunior@users.noreply.github.com>
2023-07-16 15:23:29 +0100

    Contributors page

---
+++ src/app/contributors/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Heading } from "@/components/ui/heading";
+
+
+
+export default async function ContributorsPage() {
+
+  return (
+    <div className="pt-12">
+      <Heading title="Contributors" description="All the project contributors" />
+      
+      <br/>
+      I dunno, add some more stuff here ?
+
+      <ul className="mt-3 ml-3 gap-1 list-disc">
+        <li>PatalJunior</li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
