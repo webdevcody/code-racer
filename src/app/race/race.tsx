10a94dca84e0e512da2af2ebc67659538e27ca39
Dan <87383867+scape76@users.noreply.github.com>
2023-07-16 20:24:55 +0300

    Update race.tsx
    set autoFocus instead of calling focus in useEffect

--- src/app/race/race.tsx
+++ src/app/race/race.tsx
@@ -90,7 +90,6 @@ export default function Race({ user, snippet }: RaceProps) {
       }
     };
     document.addEventListener("keydown", handleRestartKey);
-    focusOnLoad();
     return () => {
       document.removeEventListener("keydown", handleRestartKey);
     };
@@ -201,6 +200,7 @@ export default function Race({ user, snippet }: RaceProps) {
         <input
           type="text"
           // value={input}
+          autoFocus
           defaultValue={input}
           ref={inputElement}
           onKeyDown={handleKeyboardEvent}
