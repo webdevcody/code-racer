39f22640108f4b768dcb72eff477e9772970977e
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-07-17 22:56:06 +0400

    fix import name

--- src/components/achievement.tsx
+++ src/components/achievement.tsx
@@ -1,11 +1,11 @@
-import { Achievement as AchievementCard } from "@/types/achievement";
+import { Achievement } from "@/types/achievement";
 import Image from "next/image";
 import { Icons } from "./icons";
 
 const AchievementCard = ({
   achievement,
 }: {
-  achievement: Pick<AchievementCard, "image" | "name" | "description"> & {
+  achievement: Pick<Achievement, "image" | "name" | "description"> & {
     unlockedAt: Date;
   };
 }) => {
@@ -13,9 +13,11 @@ const AchievementCard = ({
     <li className="flex items-center justify-between bg-accent px-6 py-3 rounded-sm">
       <div className="flex-1 flex flex-col gap-2">
         <h3 className="text-xl font-bold">{achievement.name}</h3>
-        <p className="text-sm text-muted-foreground">
-          {achievement.description ?? "this is fine"}
-        </p>
+        {achievement.description && (
+          <p className="text-sm text-muted-foreground">
+            {achievement.description}
+          </p>
+        )}
         <p className="flex items-center text-xs text-accent-foreground">
           <Icons.trophy className="w-4 h-4 mr-2" />
           <span>
