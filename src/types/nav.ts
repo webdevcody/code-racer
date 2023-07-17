eee35e1d5bc8a6fb5d2d6233b57343ac98dd5bdf
scape76 <danyaolekhq@gmail.com>
2023-07-17 15:50:02 +0300

    dashboard config

--- src/types/nav.ts
+++ src/types/nav.ts
@@ -1,6 +1,11 @@
+import { Icons } from "@/components/icons";
+
 export interface NavItem {
   title: string;
   href?: string;
   disabled?: boolean;
   external?: boolean;
+  icon?: keyof typeof Icons;
+  label?: string;
+  description?: string;
 }
