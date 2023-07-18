0e14ccc82756e933633cd5793614aae8142c1a25
Andras Zold <andras.zold@digitalthinkers.com>
2023-07-18 13:27:13 +0200

    [399][Bug]-Change to client component, and changed the imports

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -1,10 +1,11 @@
 // Inspired by react-hot-toast library
-import * as React from "react"
+"use client"
 
 import type {
   ToastActionElement,
   ToastProps,
 } from "@/components/ui/toast"
+import { useEffect, useState } from "react"
 
 const TOAST_LIMIT = 2
 const TOAST_REMOVE_DELAY = 1000000
@@ -170,9 +171,9 @@ function toast({ ...props }: Toast) {
 }
 
 function useToast() {
-  const [state, setState] = React.useState<State>(memoryState)
+  const [state, setState] = useState<State>(memoryState)
 
-  React.useEffect(() => {
+  useEffect(() => {
     listeners.push(setState)
     return () => {
       const index = listeners.indexOf(setState)
