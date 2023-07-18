50ef013ba109edeb67a0bf516681774e50b86662
Ragudos <workemailaaronragudos@gmail.com>
2023-07-19 07:39:38 +0800

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "min-h-[50rem] h-screen flex flex-col bg-background",
+          "min-h-screen flex flex-col bg-background",
           inter.className,
           bruno_ace_sc.variable,
         )}
