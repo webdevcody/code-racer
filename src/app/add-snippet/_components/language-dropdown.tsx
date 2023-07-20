f93a3798a01a51942b51c501598c21a18d78309d
elwin212 <elwin@mmisys.net>
2023-07-20 14:47:57 -0700

    Fixed error

--- src/app/add-snippet/_components/language-dropdown.tsx
+++ src/app/add-snippet/_components/language-dropdown.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import { Check, ChevronsUpDown } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
