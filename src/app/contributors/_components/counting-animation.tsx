843d93899b0d8da3f5407f289350279e9c077f6a
Phou Kiethseng <puseng123@gmail.com>
2023-07-22 01:21:20 +0800

    [327] refactor: Improve CountingAnimation performance

--- src/app/contributors/_components/counting-animation.tsx
+++ src/app/contributors/_components/counting-animation.tsx
@@ -6,36 +6,41 @@ import { useEffect, useState, useRef } from "react";
 interface CountingAnimationProps {
   targetNumber: number;
   startingNumber?: number;
-  animationDuration?: number; // miliseconds
+  duration?: number; // miliseconds
   className?: string;
 }
+
+const UPDATE_INTERVAL = 100; // time delay between update cycle, in miliseconds
 
 export default function CountingAnimation({
   targetNumber,
   startingNumber = 0,
-  animationDuration = 3000,
+  duration = 3000,
   className,
 }: CountingAnimationProps) {
   const [currentNumber, setCurrentNumber] = useState<number>(startingNumber);
   const intervalID = useRef<any>(null);
-  const incrementDuration = Math.abs(
-    animationDuration / (targetNumber - startingNumber),
+
+  // Amount to increment in every update cycle
+  const incrementValue = Math.ceil(
+    (targetNumber - startingNumber) / (duration / UPDATE_INTERVAL),
   );
 
   function update() {
-    console.log("update invoked");
+    // console.log("update invoked");
     setCurrentNumber((val) => {
       if (val >= targetNumber) {
         clearInterval(intervalID.current);
-        return val;
+        return targetNumber;
       } else {
-        return val + 1;
+        return val + incrementValue;
       }
     });
   }
 
   useEffect(() => {
-    const ID = setInterval(update, incrementDuration);
+    // console.log("incrementValue", incrementValue);
+    const ID = setInterval(update, UPDATE_INTERVAL);
     intervalID.current = ID;
     return () => {
       clearInterval(ID);
