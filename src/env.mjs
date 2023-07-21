a34d2938e1b35e68422d552d120a0f1d7c686fb3
greidinger-reis <jp.greidinger@hotmail.com>
2023-07-21 14:24:21 -0300

    rm log

--- src/env.mjs
+++ src/env.mjs
@@ -1,7 +1,6 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-console.log(process.env)
 export const env = createEnv({
     server: {
         DATABASE_URL: z.string().url(),
