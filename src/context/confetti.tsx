7134445440076b132a6f8d8c87f3376b66d0ae8d
kos3nz <60677527+kos3nz@users.noreply.github.com>
2023-07-23 19:14:31 +0900

    fix: confetti can only invoke once without reloading
    - remove confetti context provider
    - create use-confetti.ts to enable showing confetti like the toast function

--- src/context/confetti.tsx
+++ src/context/confetti.tsx
@@ -1,41 +1,24 @@
-import React, { useContext, useState } from "react";
-import Confetti from "react-confetti";
+"use client";
 
-type ConfettiContextType = {
-  showConfetti: () => void;
-};
+import ReactConfetti from "react-confetti";
+import { useConfetti } from "./use-confetti";
 
-const ConfettiContext = React.createContext<ConfettiContextType>(
-  {} as ConfettiContextType,
-);
+export const Confetti = () => {
+  const { confetti } = useConfetti();
 
-const ConfettiProvider = ({ children }: { children: React.ReactNode }) => {
-  const [isConfettiVisible, setIsConfettiVisible] = useState(false);
-
-  const showConfetti = () => {
-    setIsConfettiVisible(true);
-  };
-
-  const confettiContextValue: ConfettiContextType = {
-    showConfetti: showConfetti,
-  };
   return (
-    <ConfettiContext.Provider value={confettiContextValue}>
-      {children}
-      {isConfettiVisible && (
-        <Confetti
+    <>
+      {confetti.map((id) => (
+        <ReactConfetti
+          key={id}
           width={window.innerWidth}
           height={window.innerHeight}
           recycle={false}
           numberOfPieces={800}
-          tweenDuration={30000}
+          tweenDuration={20000}
           initialVelocityY={-5}
         />
-      )}
-    </ConfettiContext.Provider>
+      ))}
+    </>
   );
 };
-
-const useConfettiContext = () => useContext(ConfettiContext);
-export { useConfettiContext };
-export default ConfettiProvider;
