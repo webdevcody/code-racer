427e78d94106888f34129cd51df46e015ca05cb2
Web Dev Cody <webdevcody@gmail.com>
2023-07-24 15:32:51 -0400

    new images for achievements

--- src/config/achievements.ts
+++ src/config/achievements.ts
@@ -8,12 +8,12 @@ export const achievements: Achievement[] = [
   },
   {
     type: "FIRST_SNIPPET",
-    image: "/static/first.png",
+    image: "/static/snippet.png",
     name: "First Snippet",
   },
   {
     type: "FIFTH_RACE",
-    image: "/static/first.png",
+    image: "/static/fift.png",
     name: "Fifth Race",
   },
 ];
