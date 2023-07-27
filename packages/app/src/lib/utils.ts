2d1dcaf29400ea299bfc697930ac1a6dde01ef5f
Saga-sanga <vulcan248@gmail.com>
2023-07-27 21:21:46 +0530

    Feat: Add method to convert camel case to titlecase

--- packages/app/src/lib/utils.ts
+++ packages/app/src/lib/utils.ts
@@ -49,3 +49,14 @@ export const isActiveRoute = (
   currentRouteHref: string,
   providedRouteHref: string,
 ) => currentRouteHref.startsWith(providedRouteHref);
+
+export function camelCaseToCapitalized(str: string) {
+  // Check if the string has any white spaces
+  if (/\s/.test(str)) {
+    return str;
+  }
+
+  // Insert space before each capital letter using a regular expression
+  const capitalizedStr = str.replace(/([A-Z])/g, ' $1');
+  return capitalizedStr.charAt(0).toUpperCase() + capitalizedStr.slice(1);
+}
