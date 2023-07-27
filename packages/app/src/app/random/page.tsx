bf9395a4583414f0a2c1e9d2b9d433dab64b29a9
Web Dev Cody <webdevcody@gmail.com>
2023-07-26 22:14:09 -0400

    force dynamic

--- packages/app/src/app/random/page.tsx
+++ packages/app/src/app/random/page.tsx
@@ -1,5 +1,6 @@
+export const dynamic = "force-dynamic"
 
-export default async function ContributorsPage() {
+export default async function RandomPage() {
   const response = await fetch(`${process.env.NEXTAUTH_URL}/api/random`, {
     next: {
       revalidate: 86400
