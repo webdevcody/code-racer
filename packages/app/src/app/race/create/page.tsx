0cbdea48c212d4d741c8a2714e199e9055f8c3b5
scape76 <danyaolekhq@gmail.com>
2023-07-28 19:10:25 +0300

    create room page

---
+++ packages/app/src/app/race/create/page.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface CreatePageProps {}
+
+const CreatePage: React.FC<CreatePageProps> = ({}) => {
+  return <div>Hello</div>;
+};
+
+export default CreatePage;
