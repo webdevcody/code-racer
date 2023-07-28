62e9aa27b3f4f2fb35d58031ba05e414ca643a8b
scape76 <danyaolekhq@gmail.com>
2023-07-28 21:34:02 +0300

    ui: copy button

---
+++ packages/app/src/components/ui/copy-button.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { Check, Copy } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+
+interface CopyButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  value: string
+}
+
+function copyToClipboard(value: string) {
+  navigator.clipboard.writeText(value)
+}
+
+export default function CopyButton({ value }: CopyButtonProps) {
+  const [hasCopied, setHasCopied] = useState(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setHasCopied(false), 2000)
+
+    return () => clearTimeout(timeout)
+  }, [hasCopied])
+
+  return (
+    <Button
+      type='button'
+      variant='ghost'
+      className='h-fit rounded-sm p-0 hover:bg-background'
+      onClick={() => {
+        copyToClipboard(value)
+        setHasCopied(true)
+      }}
+    >
+      <span className='sr-only'>Copy</span>
+      {hasCopied ? <Check className='h-3.5 w-3.5' /> : <Copy className='h-3.5 w-3.5' />}
+    </Button>
+  )
+}
