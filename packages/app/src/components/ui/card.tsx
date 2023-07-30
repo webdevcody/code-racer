713712c45ac2b1ae89a50fa013aa7580f8c06e64
awfulminded <mathsteam11@gmail.com>
2023-07-30 21:11:32 +0300

    Contributors page UI improvements

--- packages/app/src/components/ui/card.tsx
+++ packages/app/src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border dark:border-slate-700 dark:bg-slate-900 bg-white border-slate-200 text-card-foreground shadow-sm",
       className,
     )}
     {...props}
