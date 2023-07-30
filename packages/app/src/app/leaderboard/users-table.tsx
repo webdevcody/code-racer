29702d41970ae8314fcb3cd0ea801af3746d1bfb
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-07-30 18:01:52 +0100

    put defaultSorting prop back to descending

--- packages/app/src/app/leaderboard/users-table.tsx
+++ packages/app/src/app/leaderboard/users-table.tsx
@@ -160,7 +160,7 @@ export function UsersTable({
         pageCount={pageCount}
         defaultSorting={{
           prop: "averageCpm",
-          val: "asc",
+          val: "desc",
         }}
       />
       <p className="text-sm md:text-base mt-1 text-muted-foreground">
