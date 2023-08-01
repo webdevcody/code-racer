b87abef4a266a4a94912dc926fc8447f98646cd3
Phou Kiethseng <puseng123@gmail.com>
2023-08-02 00:36:52 +0800

    Fix practice page test failing

--- packages/app/cypress/e2e/tests/TestPracticeRace.cy.js
+++ packages/app/cypress/e2e/tests/TestPracticeRace.cy.js
@@ -17,22 +17,23 @@ const TIME_TO_WAIT = 1000;
 
 it("can successfully completed a practice race", () => {
   // Find Race Navigation and click on it
-  navbarComponent.race().should('be.visible').click();
+  navbarComponent.race().should("be.visible").click();
 
   // Find language selection and type snippet language
-  racePage.languageDropdown().should('be.visible').click();
-  racePage.htmlLanguageOption().should('be.visible').click();
+  racePage.practiceCardlanguageDropdown().should("be.visible").click();
+  racePage.htmlLanguageOption().should("be.visible").click();
 
   // Find practice button to start practice race
   racePage.practiceButton().click();
 
   // Get code as text, and use that to type into input
-  racePage.codeSnippet()
+  racePage
+    .codeSnippet()
     .children()
     .then((spans) => {
       RaceBL.runTypingRace(spans);
     });
-  cy.wait(TIME_TO_WAIT)
+  cy.wait(TIME_TO_WAIT);
 
   cy.url().should("include", "/result");
 });
