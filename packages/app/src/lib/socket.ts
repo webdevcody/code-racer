a9aa100c29c060d7349ebdcd2cf354c2b358e3c9
scape76 <danyaolekhq@gmail.com>
2023-08-05 19:48:41 +0300

    fix: change socket connection url

--- packages/app/src/lib/socket.ts
+++ packages/app/src/lib/socket.ts
@@ -4,6 +4,6 @@ import type { ServerToClientEvents } from "@code-racer/wss/src/events/server-to-
 import type { Socket } from "socket.io-client";
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-  "http://localhost:3001",
+  process.env.NEXT_PUBLIC_WSS_URL!,
   { transports: ["websocket"] },
 );
