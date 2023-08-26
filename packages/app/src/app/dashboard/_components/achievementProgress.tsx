cc74359903830f65d446497b08846d4c84dd8ee7
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-08-27 00:52:54 +0200

    remove the intersection observer from achievementProgress, removed timeline.css file and rewrite its code to tailwindCss code, implemented achievement schine animation in achievement and achievement components

--- packages/app/src/app/dashboard/_components/achievementProgress.tsx
+++ packages/app/src/app/dashboard/_components/achievementProgress.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { BanIcon } from "lucide-react";
 import { Achievement as PrsimaAchievement } from "@prisma/client";
 import { Achievement } from "@/types/achievement";
-import "../timeline.css";
 
 interface Props {
   userAchievements: PrsimaAchievement[];
@@ -34,36 +33,8 @@ export default function AchievementProgress({
   return (
     <ScrollArea className="rounded-md h-[300px] border-2 border-primary p-4 mt-10">
       <h3 className="text-primary font-special">Achievement Progress</h3>
-      <div className="timeline">
+      <div className="relative my-[20px] flex flex-col justify-center items-center">
         {updatedAllAch.map((achievement) => {
-          const timelineItemRef = useRef<HTMLDivElement>(null);
-
-          const [isVisible, setIsVisible] = useState<boolean>(false);
-
-          useEffect(() => {
-            const observer = new IntersectionObserver(
-              (entries) => {
-                const [entry] = entries;
-                setIsVisible(entry.isIntersecting);
-              },
-              {
-                root: null,
-                rootMargin: "-20px",
-                threshold: 0.1, // Adjust this threshold as per your needs
-              }
-            );
-
-            if (timelineItemRef.current) {
-              observer.observe(timelineItemRef.current);
-            }
-
-            return () => {
-              if (timelineItemRef.current) {
-                observer.unobserve(timelineItemRef.current);
-              }
-            };
-          }, [timelineItemRef]);
-
           const isUnlocked = userAchievements.some(
             (userAchievement) =>
               userAchievement.achievementType === achievement.type
@@ -73,14 +44,20 @@ export default function AchievementProgress({
 
           return (
             <div
-              ref={timelineItemRef}
               key={achievement.type}
               id={itemId}
-              className={`timeline-item ${isUnlocked ? "unlocked" : "locked"} ${
-                isVisible && "animate-fade"
-              } shadow-lg shadow-accent opacity-100 `}
+              style={
+                isUnlocked
+                  ? {
+                      backgroundImage: "var(--achievement-progress-gradient)",
+                    }
+                  : {}
+              }
+              className={`relative h-[62px] p-[3px] mb-[20px] rounded-sm  cursor-pointer w-1/2 lg:w-1/4  ${
+                isUnlocked ? "animate-gradient bg-achievement" : "bg-red-500"
+              }  shadow-lg shadow-accent even:ml-[50%] odd:mr-[50%]`}
             >
-              <div className="timeline-content">
+              <div className="flex items-center justify-center text-center w-full h-full bg-accent rounded-md">
                 {isUnlocked ? (
                   <Avatar>
                     <AvatarImage src={achievement.image} />
@@ -88,7 +65,7 @@ export default function AchievementProgress({
                 ) : (
                   <BanIcon className="text-primary" />
                 )}
-                <p>{achievement.name}</p>
+                <p className="m-0 pl-[3px]">{achievement.name}</p>
               </div>
             </div>
           );
