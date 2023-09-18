7eb2217b0026c459040f30bd8ebea624f5f2f95b
Sushil Magare <sushilmagare10>
2023-09-18 22:18:14 +0530

    custom 404 Page

---
+++ packages/app/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import { Typing } from "./typing"
+
+function Notfound() {
+    return (
+        <div className=" flex h-[80vh] items-center justify-center">
+            <Typing />
+        </div>
+    )
+
+}
+
+export default Notfound
