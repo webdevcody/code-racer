f4380565d7756544cba8bcb49eae3f4e65d6019e
ParadigmMango2 <jacobjhartt@gmail.com>
2024-04-07 16:15:54 -0600

    Implemented a basic login test

--- packages/app/cypress/e2e/tests/TestLogin.cy.js
+++ packages/app/cypress/e2e/tests/TestLogin.cy.js
@@ -4,7 +4,13 @@ beforeEach(() => {
   cy.visit("/");
 });
 
-it("can rach the login page and access github API", () => {
-  
+it("can reach the login page and access github API", () => {
+  // cy.contains('button', 'Sign in').click()
+  cy.get('[data-cy="sign in"]').click()
+
+  // cy.url().should('include', 'github.com/login')
+  cy.on("url:changed", (newUrl) => {
+    expect(newUrl).to.contain("github.com/login")
+  })
 });
 
